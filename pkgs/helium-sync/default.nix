48362655e4ab4926b0457ce149015a1ec7cdd7f4
imnyang <imnyang@pm.me>
2026-04-26 03:41:27 +0900

    wow

--- pkgs/helium-sync/default.nix
+++ pkgs/helium-sync/default.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "helium-sync";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "imnyang";
     repo = "helium-sync";
     rev = "v${version}";
-    hash = "sha256-Iml9UjCRN2o7cR+IJR72ujQ8sCW8xjzwhHal3NqpuVo=";
+    hash = "sha256-ARd2S9c5iFVvSwFTyZjEB2w1l54vvYBXKZTESWpCqvw=";
   };
 
   vendorHash = "sha256-sWi8QV1uMjfgRMATjXb/qCp6IvBQojtLI3Gr2BHS9Hs=";
