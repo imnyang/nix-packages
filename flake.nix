eceaac05131da19da0714c1c0265cebddbb18e30
imnyang <imnyang@pm.me>
2026-04-26 02:11:42 +0900

    wow

--- flake.nix
+++ flake.nix
@@ -14,13 +14,14 @@
       waterfox-bin = final.callPackage ./pkgs/waterfox-bin/default.nix { };
       xcursor-mizuki = final.callPackage ./pkgs/xcursor-mizuki/default.nix { stdenv = final.stdenv; };
       pjsk-cursor = final.callPackage ./pkgs/pjsk-cursor/default.nix { stdenv = final.stdenv; };
+      helium = final.callPackage ./pkgs/helium/default.nix { };
     };
     pkgs = nixpkgs.legacyPackages.${system}.extend overlay;
   in {
     overlays.default = overlay;
 
     packages.${system} = {
-      inherit (pkgs) waterfox-bin xcursor-mizuki pjsk-cursor;
+      inherit (pkgs) waterfox-bin xcursor-mizuki pjsk-cursor helium;
       # default = pkgs.waterfox-bin;
     };
   };
---
+++ pkgs/helium/default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  commandLineArgs ? [ ],
+  enableFeatures ? [ ],
+  libvaSupport ? pkgs.stdenv.hostPlatform.isLinux,
+  widevineSupport ? false,
+}:
+pkgs.appimageTools.wrapType2 rec {
+
+  pname = "helium";
+  version = "0.11.4.1";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
+    sha256 = "sha256-EavHZuleVkgD4J+gcUQ2ZgGHjvWXzdamUh7r42bUkas=";
+  };
+
+  _enableFeatures =
+    enableFeatures
+    ++ pkgs.lib.optionals libvaSupport [
+      "VaapiVideoDecoder"
+    ];
+
+  extraPkgs = pkgs: pkgs.lib.optionals libvaSupport [ pkgs.libva ];
+
+  extraBwrapArgs = [
+    "--ro-bind-try /etc/chromium /etc/chromium"
+  ];
+
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
+  extraInstallCommands =
+    let
+      contents = pkgs.appimageTools.extract { inherit pname version src; };
+    in
+    ''
+      wrapProgram $out/bin/${pname} \
+        ${
+          pkgs.lib.optionalString (
+            _enableFeatures != [ ]
+          ) "--add-flags \"--enable-features=${pkgs.lib.strings.concatStringsSep "," _enableFeatures}\""
+        } \
+        ${pkgs.lib.optionalString (
+          commandLineArgs != [ ]
+        ) "--add-flags \"${pkgs.lib.strings.concatStringsSep " " commandLineArgs}\""} \
+        ${pkgs.lib.optionalString widevineSupport ''
+          --run "mkdir -p ~/.config/net.imput.helium/WidevineCdm" \
+          --run "echo '{\"Path\":\"${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm\"}' > ~/.config/net.imput.helium/WidevineCdm/latest-component-updated-widevine-cdm"
+        ''}
+      install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
+
+}
