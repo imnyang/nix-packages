dcd93e6d8cd488fec9d444be0d7b41c1955c98c8
mizuki <akiyama@mizuki.guru>
2026-04-29 09:01:26 +0900

    pkgs/helium: update to 0.11.6.1

--- pkgs/helium/default.nix
+++ pkgs/helium/default.nix
@@ -8,11 +8,11 @@
 pkgs.appimageTools.wrapType2 rec {
 
   pname = "helium";
-  version = "0.11.5.1";
+  version = "0.11.6.1";
 
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-    sha256 = "sha256-Ni7IZ9UBafr+ss0BcQaRKqmlmJI4IV1jRAJ8jhcodlg=";
+    sha256 = "sha256-NvsSVeKr82ME8oPupU8Oyh9IbYerxAWJ5vRjvj4WyLo=";
   };
 
   _enableFeatures =
