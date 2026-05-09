c17454d554791f1d622972bf3bf77be8b1f5fd26
imnyang <imnyang@pm.me>
2026-05-09 12:40:22 +0900

    Update pkgs/vscode-insiders/default.nix

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -3,7 +3,7 @@
 let
   src = fetchTarball {
     url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-    sha256 = "0cq5p2r949k1nskfacz2j0m9zyg116zpzc8csmxb1q51p7znixkc"; 
+    sha256 = "1v1r8vq41dmxaifqhfhgji12qwgm44xanwbihrv3319384s8bf5f"; 
   };
 in
 (pkgs.vscode.override {
