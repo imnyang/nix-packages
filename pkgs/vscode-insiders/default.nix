4ea2f6c218a8f523843845fa8860c06d2bc6b899
imnyang <imnyang@pm.me>
2026-05-25 21:57:24 +0900

    Update pkgs/vscode-insiders/default.nix

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -16,11 +16,15 @@ in
 
   buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
 
-
   prePatch = ''
     ${oldAttrs.prePatch or ""}
     mkdir -p resources/app/node_modules/@vscode/ripgrep/bin
     touch resources/app/node_modules/@vscode/ripgrep/bin/rg
+  '';
+
+  preFixup = ''
+    ${oldAttrs.preFixup or ""}
+    rm -rf resources/app/node_modules/@github/copilot-linuxmusl-x64
   '';
 
   meta = (oldAttrs.meta or {}) // {
