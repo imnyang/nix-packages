3df4a4c0559c5242ec9e9d4c71d640721eca2a27
imnyang <imnyang@pm.me>
2026-05-26 09:21:58 +0900

    wow

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -1,9 +1,23 @@
 { pkgs }:
 
 let
-  src = fetchTarball {
+  inherit (pkgs) stdenv;
+
+  sysAttrs = if stdenv.hostPlatform.isDarwin then 
+    if stdenv.hostPlatform.isAarch64 then {
+      url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-arm64-dmg";
+      sha256 = "sha256-K+U6JtVNz1PRzL/0SIEK7RXwQu6LL3p+248Ywbn+mDc=";
+    } else {
+      url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-x64-dmg";
+      sha256 = "sha256-ex0ESS9APZxgc7b96Q7EQbckUXE51LmTqDfsnN07W3I=";
+    }
+  else {
     url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-    sha256 = "04brczgkvpkqrg018wss9z5hk56n4d58r1bd7zq83wsycma9mq2b"; 
+    sha256 = "sha256-kE99FyilwwFv3zTCkufGwD3s1WwoJ2krg10l3Xy5PRE=";
+  };
+
+  src = fetchTarball {
+    inherit (sysAttrs) url sha256;
   };
 in
 (pkgs.vscode.override {
@@ -14,20 +28,23 @@ in
   isInsiders = true;
   inherit src;
 
-  buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
+  buildInputs = (oldAttrs.buildInputs or []) 
+    ++ stdenv.lib.optionals stdenv.hostPlatform.isLinux [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
 
-  prePatch = ''
+  prePatch = stdenv.lib.optionalString stdenv.hostPlatform.isLinux ''
     ${oldAttrs.prePatch or ""}
     mkdir -p resources/app/node_modules/@vscode/ripgrep/bin
     touch resources/app/node_modules/@vscode/ripgrep/bin/rg
   '';
 
-  preFixup = ''
+  preFixup = if stdenv.hostPlatform.isDarwin then ''
+    ${oldAttrs.preFixup or ""}
+  '' else ''
     ${oldAttrs.preFixup or ""}
     rm -rf resources/app/node_modules/@github/copilot-linuxmusl-x64
   '';
 
   meta = (oldAttrs.meta or {}) // {
-    mainProgram = "code-insiders";
+    mainProgram = if stdenv.hostPlatform.isDarwin then "code-insiders" else "code-insiders";
   };
 })
